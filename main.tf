a587d7e241df5b02481fa6be66b475a060d08872
David Inostroza <dinostrozars@gmail.com>
2023-10-09 18:26:05 -0300

    Update repositories and attributes

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 terraform {
   backend "s3" {
-    bucket = "YOUR_BUCKET"
-    key    = "YOUR_KEY"
-    region = "YOUR_REGION"
+    bucket = "terraform-state-airline-msur"
+    key    = "terraform/backend"
+    region = "us-east-1"
   }
 }
 
 locals {
   env_name         = "staging"
-  aws_region       = "YOUR_REGION"
+  aws_region       = "us-east-1"
   k8s_cluster_name = "ms-cluster"
+
 }
 
 variable "mysql_password" {
@@ -22,11 +23,11 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "msur" {
-  name              = module.aws-kubernetes-cluster.eks_cluster_id
+  name = module.aws-kubernetes-cluster.eks_cluster_id
 }
 
 module "aws-network" {
-  source = "github.com/implementing-microservices/module-aws-network"
+  source = "github.com/dinostroza/module-aws-network"
 
   env_name              = local.env_name
   vpc_name              = "msur-VPC"
@@ -40,7 +41,7 @@ module "aws-network" {
 }
 
 module "aws-kubernetes-cluster" {
-  source = "github.com/implementing-microservices/module-aws-kubernetes"
+  source = "github.com/dinostroza/module-aws-kubernetes"
 
   ms_namespace       = "microservices"
   env_name           = local.env_name
@@ -60,7 +61,7 @@ module "aws-kubernetes-cluster" {
 # Create namespace
 # Use kubernetes provider to work with the kubernetes cluster API
 provider "kubernetes" {
-  load_config_file       = false
+  # load_config_file       = false
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
   host                   = data.aws_eks_cluster.msur.endpoint
   exec {
@@ -78,9 +79,8 @@ resource "kubernetes_namespace" "ms-namespace" {
 }
 
 module "argo-cd-server" {
-  source = "github.com/implementing-microservices/module-argo-cd"
+  source = "github.com/dinostroza/module-argo-cd"
 
-  aws_region            = local.aws_region
   kubernetes_cluster_id = data.aws_eks_cluster.msur.id
 
   kubernetes_cluster_name      = module.aws-kubernetes-cluster.eks_cluster_name
@@ -91,7 +91,7 @@ module "argo-cd-server" {
 }
 
 module "aws-databases" {
-  source = "github.com/implementing-microservices/module-aws-db"
+  source = "github.com/dinostroza/module-aws-db"
 
   aws_region     = local.aws_region
   mysql_password = var.mysql_password
@@ -105,7 +105,7 @@ module "aws-databases" {
 }
 
 module "traefik" {
-  source = "github.com/implementing-microservices/module-aws-traefik/"
+  source = "github.com/dinostroza/module-aws-traefik"
 
   aws_region                   = local.aws_region
   kubernetes_cluster_id        = data.aws_eks_cluster.msur.id
